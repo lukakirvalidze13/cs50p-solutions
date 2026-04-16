334cc67e201f72d9ee638aa1f23e8b1c2eff40e1
Lukakirvalidze13 <lukayt1996@gmail.com>
2026-04-17 00:00:33 +0200

    new

---
+++ hw1.py
@@ -0,0 +1,5 @@
+# Write a program that prompts the user for a string and then outputs that same string in lowercase.
+
+# Ask user to write a letter
+letter = input("Type any letter: ").lower()
+print(letter)
