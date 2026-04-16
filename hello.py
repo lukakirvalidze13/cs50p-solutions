f5a6c40636000e83bda455f74e189ae8fea52909
Lukakirvalidze13 <lukayt1996@gmail.com>
2026-04-16 23:56:35 +0200

    New

---
+++ hello.py
@@ -0,0 +1,86 @@
+# print("Hello world")
+
+# FUNCTIONS = reusable blocks of code that perform a specific task
+# They can take inputs (parameters) and may return a value
+
+# Examples:
+# print()        → built-in (no return)
+# len()          → built-in (returns value)
+# my_function()  → custom function
+
+
+
+# ARGUMENTS = values passed into a function when calling it
+#Example - print("Hello")   # "Hello" is the argument
+
+#def greet(name):
+#    print(f"Hello, {name}")
+#greet("Alex")    # "Alex" is the argument
+
+
+#Bug = Mistakes / Problems to solve.
+#Example = print("Hello world"  - Parenthesis is missing
+
+# input("Do you like cars? ")
+# print("I knew you loved BMW's :)")
+
+
+#Variables
+#Just a container for a value inside a computer that can store images, texts, numbers etc...
+
+# print("Car reliability checker")
+# reliable = input("What model are you looking for ")
+# print(reliable, "is extremely reliable if well maintained")
+
+# #Pseudocode. is very useful before writing the code, it turns code into smaller sized chunks/tasks
+
+# #Ask user for their name
+# name = input("What's your name? ")
+
+# #Say hi to the user
+# print("Hello, " + name)
+
+
+#DATA TYPES
+
+# str - sequence of text (string)
+
+
+
+# name = input("What's your name? ")
+# name = name.strip() #remove whitespace from str. even if user types in space it will return without spaces
+# # name = name.capitalize() #capitalize first word
+# # name = name.title() capitalizes title style, every word
+# print(f"Hello, {name}") # f-string lets you put variables inside text easily
+
+
+# Parameters = variables inside a function definition
+# They receive values (arguments) when the function is called
+# def greet(name):   # name = parameter
+#     print(name)
+
+# greet("Alex")      # "Alex" = argument
+
+
+# sep = separator between values (default is space " ")
+# end = what is printed at the end (default is newline "\n")
+
+
+# print("Hello there", "\"Hii\"")
+
+
+# # Ask user for their name
+# name = input("What's your name? ").strip().title()
+
+# # # Remove whitespace from str and 
+# # name = name.strip()
+
+# # #capitalize user's name
+# # name = name.title()
+
+# #split user's name into first and last name
+# first, last = name.split(" ")
+
+# # Say hello to the user
+# print(f"Hello, {first}")
+
